d7344b316af5de019142607b902389e6339fff4b
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-03-11 20:07:03 +0100

    Uppdaterat vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+}) */
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/jobchaser-react/',
+});
